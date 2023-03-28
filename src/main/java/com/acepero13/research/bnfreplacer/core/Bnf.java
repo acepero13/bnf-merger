94959ec4cfab18655eac182310b0030e4fed1f98
Alvaro <acepero13@gmail.com>
2023-03-28 20:35:21 +0200

    Update Bnf.java

--- src/main/java/com/acepero13/research/bnfreplacer/core/Bnf.java
+++ src/main/java/com/acepero13/research/bnfreplacer/core/Bnf.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Bnf implements Iterable<Expression> {
+final class Bnf implements Iterable<Expression> {
     private final List<Expression> expressions;
     private List<Expression> mutableExpressions;
 
